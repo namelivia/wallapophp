9ce137803801883d02fef0e73d062b28c23226fa
José Ignacio Amelivia <ohcan2@gmail.com>
2019-01-06 21:29:47 +0100

    Adding symfony var-dumper cause I love it, and using it on the demo script

--- demo.php
+++ demo.php
@@ -6,31 +6,19 @@ and also for using it as an acceptance test.
 require __DIR__.'/vendor/autoload.php';
 $client = \Namelivia\Wallapophp\ServiceProvider::build();
 echo 'User method:';
-echo "\n";
-echo $client->user('40000000');
-echo "\n";
+var_dump($client->user('40000000'));
 
 echo 'User received:';
-echo "\n";
-echo $client->userReviewsReceived('40000000');
-echo "\n";
+var_dump($client->userReviewsReceived('40000000'));
 
 echo 'User sent:';
-echo "\n";
-echo $client->userReviewsSent('40000000');
-echo "\n";
+var_dump(json_decode($client->userReviewsSent('40000000')));
 
 echo 'User items published:';
-echo "\n";
-echo $client->userItemsPublished('40000000');
-echo "\n";
+var_dump(json_decode($client->userItemsPublished('40000000')));
 
 echo 'User items sold:';
-echo "\n";
-echo $client->userItemsSold('40000000');
-echo "\n";
+var_dump(json_decode($client->userItemsSold('40000000')));
 
 echo 'Search';
-echo "\n";
-echo $client->search('40.24', '3.41');
-echo "\n";
+var_dump(json_decode($client->search('40.24', '3.41', 'Dreamcast')));
